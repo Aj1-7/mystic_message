495da69e84fff8f8d99fbf2116ded96ecb9321b1
Aj1-7 <arjunpandita171@gmail.com>
2025-06-06 00:50:23 +0530

    working on frontend shadcn backend completed almost except that openai api

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,50 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import React,{useState,useEffect} from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useDebounceValue } from "usehooks-ts"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button className = "bg-orange-400 px-3 py-1 m-4" onClick={() => signIn()}>Sign in</button>
-    </>
+
+
+const page  = ()=>{
+  const [username,setUsername] = useState('')
+  const [usernameMessage,setUsernameMessage] = useState('')
+  const [isCheckingUsername,setIsCheckingUsername] = useState(false)
+  const [isSubmitting,setIsSubmitting] = useState(false)
+  const debouncedUsername = useDebounceValue(username,500)  
+  const router = useRouter();
+  
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',  
+      password: '',
+    },
+  });
+
+  useEffect(()=>{
+    if(debouncedUsername.length > 0){
+      setIsCheckingUsername(true)
+      setUsernameMessage('')
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+  },[debouncedUsername])
+  
+
+  
+  return(
+    <div>page</div>
   )
 }
+
+export default page;
